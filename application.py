389cebd552861d454021f3d1e806bd76ab165aac
Violeta Calvo <violeta.itt@gmail.com>
2016-09-24 21:02:01 +0200

    configure application.py to use postgresql

--- application.py
+++ application.py
@@ -20,7 +20,7 @@ APPLICATION_NAME = "Item Catalog app"
 
 
 # Connect to Database and create database session
-engine = create_engine('sqlite:///itemcatalog.db')
+engine = create_engine('postgresql:///catalog')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
