3dde62ccf833388c6b5dc828f8220b7949290afc
Violeta Calvo <violeta.itt@gmail.com>
2016-09-24 20:42:03 +0200

    config.sh to install python modules

---
+++ config.sh
@@ -0,0 +1,11 @@
+apt-get -qqy install python-psycopg2
+apt-get -qqy install python-flask python-sqlalchemy
+apt-get -qqy install python-pip
+pip install bleach
+pip install oauth2client
+pip install requests
+pip install httplib2
+pip install redis
+pip install passlib
+pip install itsdangerous
+pip install flask-httpauth
